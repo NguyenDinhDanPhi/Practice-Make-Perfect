e465a23cbb0fe5cf071f9223c279322f4a7d232b
danphi <phittnh@gmail.com>
2025-01-23 17:42:08 +0700

    create prj bankey

---
+++ Bankey/Bankey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by dan phi on 23/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
