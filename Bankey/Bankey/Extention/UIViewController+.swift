a4f40f428d993035875931a51afa14bc75f364f1
danphi <phittnh@gmail.com>
2025-01-25 21:20:34 +0700

    change UINavigationBarAppearance

--- Bankey/Bankey/Extention/UIViewController+.swift
+++ Bankey/Bankey/Extention/UIViewController+.swift
@@ -8,18 +8,14 @@
 import UIKit
 
 extension UIViewController {
-    
     func setStatusBar() {
-        var statusBarSize: CGSize {
-            return UIApplication.shared.connectedScenes
-                .compactMap { $0 as? UIWindowScene }
-                .first?.statusBarManager?.statusBarFrame.size ?? .zero
+            let navBarAppearance = UINavigationBarAppearance()
+            navBarAppearance.configureWithTransparentBackground() // to hide Navigation Bar Line also
+        navBarAppearance.backgroundColor = .systemTeal
+            UINavigationBar.appearance().standardAppearance = navBarAppearance
+            UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
         }
-        let frame = CGRect(origin: .zero, size: statusBarSize)
-        let statusBarView = UIView(frame: frame)
-        view.addSubview(statusBarView)
-    }
-    
+        
     func setTabBarImage(imageName: String, title: String) {
         let config = UIImage.SymbolConfiguration(scale: .large)
         let img = UIImage(systemName: imageName, withConfiguration: config)
