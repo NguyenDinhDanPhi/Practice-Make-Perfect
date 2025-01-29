9a421c147167371def67dd06448242e0ef6e357d
danphi <phittnh@gmail.com>
2025-01-29 13:58:32 +0700

    refactor SceneDelegate

--- Bankey/Bankey/SceneDelegate.swift
+++ Bankey/Bankey/SceneDelegate.swift
@@ -8,10 +8,11 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
     let loginViewController = LoginViewController()
     let onboardingContainerViewController = OnboardingContainerViewController()
+    let onboardingVC = OnboardingContainerViewController()
     let mainVC = MainViewController()
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -21,15 +22,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let window = UIWindow(windowScene: windowScreen)
         loginViewController.delegate = self
         onboardingContainerViewController.delegate = self
-        mainVC.setStatusBar()
-        UINavigationBar.appearance().isTranslucent = false
-        UINavigationBar.appearance().backgroundColor = .red
-        window.rootViewController = mainVC
-        window.makeKeyAndVisible()
+        displayLogin()
         self.window = window
     }
     
-    func setRootView(_ vc: UIViewController, animated: Bool = true) {
+    private func displayLogin() {
+        setRootViewController(loginViewController)
+    }
+    
+    func setRootViewController(_ vc: UIViewController, animated: Bool = true) {
         guard animated, let window = self.window else {
             self.window?.rootViewController = vc
             self.window?.makeKeyAndVisible()
@@ -38,31 +39,46 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window.rootViewController = vc
         window.makeKeyAndVisible()
-        UIView.transition(with: window, duration: 0.3, options: .transitionCrossDissolve, animations: nil, completion: nil)
+        UIView.transition(with: window,
+                          duration: 0.3,
+                          options: .transitionCrossDissolve,
+                          animations: nil,
+                          completion: nil)
     }
-
-
+    
+    private func prepMainView() {
+        mainVC.setStatusBar()
+        UINavigationBar.appearance().isTranslucent = false
+        UINavigationBar.appearance().backgroundColor = .systemTeal
+    }
+    
+    private func displayNextScreen() {
+        if LocalStorage.hasOnboarded {
+            prepMainView()
+            setRootViewController(mainVC)
+        } else {
+            setRootViewController(onboardingVC)
+        }
+    }
+    
 }
 //MARK: DELEGATE
 
 extension SceneDelegate: LoginViewControllerDelegate, OnboardingContainerViewControllerDelegate, LogoutDelegate {
+    
     func didLogout() {
-        setRootView(loginViewController)
-
+        setRootViewController(loginViewController)
+        
     }
     
     func didFisnishOnboarding() {
         LocalStorage.hasOnboarded = true
-        setRootView(mainVC)
+        prepMainView()
+        setRootViewController(mainVC)
     }
     
     func didLogin() {
-        if LocalStorage.hasOnboarded {
-            setRootView(mainVC)
-        } else {
-            setRootView(onboardingContainerViewController)
-        }
+        displayNextScreen()
     }
-    
     
 }
