0bcaa7028a7b4eb139d8c7244dab8076d90f7027
danphi <phittnh@gmail.com>
2025-02-17 16:20:54 +0700

    some unitTest

--- Learning Reactive/Tip-learning Reactive and more/Tip-learning Reactive and moreTests/Tip_learning_Reactive_and_moreTests.swift
+++ Learning Reactive/Tip-learning Reactive and more/Tip-learning Reactive and moreTests/Tip_learning_Reactive_and_moreTests.swift
@@ -42,6 +42,61 @@ final class Tip_learning_Reactive_and_moreTests: XCTestCase {
         
     }
     
+    func testResultWithoutTipfor2Person() {
+        // given
+        let bill: Double = 100.0
+        let tip: Tip = .none
+        let split: Int = 2
+        // when
+        let input = buildInput(bill: bill, tip: tip, split: split)
+        let output = sut.transform(input: input)
+        // then
+        output.upDateViewPublisher.sink { rs in
+            XCTAssertEqual(rs.totalBill, 100.0)
+            XCTAssertEqual(rs.totaltTip, 0)
+            XCTAssertEqual(rs.amountPerPerson, 50.0)
+            
+        }.store(in: &cancelable)
+        
+    }
+    
+    func testResultWith10PercenTipfor2Person() {
+        // given
+        let bill: Double = 100.0
+        let tip: Tip = .tenPercent
+        let split: Int = 2
+        // when
+        let input = buildInput(bill: bill, tip: tip, split: split)
+        let output = sut.transform(input: input)
+        // then
+        output.upDateViewPublisher.sink { rs in
+            XCTAssertEqual(rs.totalBill, 110.0)
+            XCTAssertEqual(rs.totaltTip, 10.0)
+            XCTAssertEqual(rs.amountPerPerson, 55.0)
+            
+        }.store(in: &cancelable)
+        
+    }
+    
+    func testResultWithCustomTipTipfor2Person() {
+        // given
+        let bill: Double = 100.0
+        let tip: Tip = .custom(value: 100)
+        let split: Int = 2
+        // when
+        let input = buildInput(bill: bill, tip: tip, split: split)
+        let output = sut.transform(input: input)
+        // then
+        output.upDateViewPublisher.sink { rs in
+            XCTAssertEqual(rs.totalBill, 200.0)
+            XCTAssertEqual(rs.totaltTip, 100.0)
+            XCTAssertEqual(rs.amountPerPerson, 100.0)
+            
+        }.store(in: &cancelable)
+        
+    }
+
+    
     func buildInput(bill: Double, tip: Tip, split: Int) -> CalculatorTipViewModel.InPut {
         return .init(billPublisher: Just(bill).eraseToAnyPublisher(),
                      tipPublisher: Just(tip).eraseToAnyPublisher(),
