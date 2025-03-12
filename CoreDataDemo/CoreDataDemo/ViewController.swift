ce4445e8bdc00fdd3ea84e1d529f96007f588ece
danphi <phittnh@gmail.com>
2025-03-12 23:13:53 +0700

    add CoreDataDemo prj

---
+++ CoreDataDemo/CoreDataDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by dan phi on 12/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
