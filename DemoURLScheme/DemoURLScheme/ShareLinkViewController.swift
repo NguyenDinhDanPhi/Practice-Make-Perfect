e24575a5b8fe209983c16b6406de9c493062bd2d
danphi <phittnh@gmail.com>
2025-03-27 10:18:47 +0700

    refator

--- DemoURLScheme/DemoURLScheme/ShareLinkViewController.swift
+++ DemoURLScheme/DemoURLScheme/ShareLinkViewController.swift
@@ -1,9 +1,18 @@
 import UIKit
-import Photos
-
 class ShareLinkViewController: UIViewController {
-    
     private let shareLink: String
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: 50, height: 68)
+        layout.minimumInteritemSpacing = 8
+        layout.minimumLineSpacing = 8
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.backgroundColor = .clear
+        return collectionView
+    }()
     
     init(shareLink: String) {
         self.shareLink = shareLink
@@ -13,61 +22,6 @@ class ShareLinkViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    // Hàm kiểm tra ứng dụng có được cài đặt không
-    private func isAppInstalled(urlScheme: String) -> Bool {
-        if let url = URL(string: urlScheme) {
-            return UIApplication.shared.canOpenURL(url)
-        }
-        return false
-    }
-    
-    // Danh sách tất cả các ứng dụng
-    private lazy var allShareItems: [SocialNetworkSharing] = [
-        SocialNetworkSharing(icon: UIImage(named: "copyLink"), title: "Copy", action: copyLinkToClipboard),
-        SocialNetworkSharing(icon: UIImage(named: "fb"), title: "Facebook", action: openFacebook),
-        SocialNetworkSharing(icon: UIImage(named: "mess"), title: "Messenger", action: shareToMessenger),
-        SocialNetworkSharing(icon: UIImage(named: "tele"), title: "Telegram", action: openTelegram),
-        SocialNetworkSharing(icon: UIImage(named: "insta"), title: "Instagram", action: shareToInstagram),
-        SocialNetworkSharing(icon: UIImage(named: "sms"), title: "SMS", action: shareToSMS),
-        SocialNetworkSharing(icon: UIImage(systemName: "hehe"), title: "X", action: shareToTwitter),
-        SocialNetworkSharing(icon: UIImage(systemName: "more"), title: "More", action: openActivityView)
-        
-        
-    ]
-    
-    // Lọc chỉ giữ lại những ứng dụng đã cài đặt
-    private lazy var shareItems: [SocialNetworkSharing] = {
-        return allShareItems.filter { item in
-            switch item.title {
-            case "Facebook":
-                return isAppInstalled(urlScheme: "fb://")
-            case "Messenger":
-                return isAppInstalled(urlScheme: "fb-messenger://")
-            case "Telegram":
-                return isAppInstalled(urlScheme: "tg://")
-            case "Instagram":
-                return isAppInstalled(urlScheme: "instagram://")
-            case "X":
-                return isAppInstalled(urlScheme: "twitter://")
-            default:
-                return true // Copy luôn hiển thị
-            }
-        }
-    }()
-    
-    private let collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        layout.itemSize = CGSize(width: 50, height: 68)
-        layout.minimumInteritemSpacing = 8
-        layout.minimumLineSpacing = 8
-        
-        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        cv.showsHorizontalScrollIndicator = false
-        cv.backgroundColor = .clear
-        return cv
-    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -82,6 +36,7 @@ class ShareLinkViewController: UIViewController {
         
         view.addSubview(collectionView)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
         NSLayoutConstraint.activate([
             collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
@@ -90,93 +45,70 @@ class ShareLinkViewController: UIViewController {
             collectionView.heightAnchor.constraint(equalToConstant: 68)
         ])
     }
-}
+    
+    private func isAppInstalled(urlScheme: String) -> Bool {
+        guard let url = URL(string: urlScheme) else { return false }
+        return UIApplication.shared.canOpenURL(url)
+    }
+    
+    private lazy var shareItems: [SocialNetworkSharing] = {
+        let allItems: [SocialNetworkSharing] = [
+            SocialNetworkSharing(icon: UIImage(named: "copyLink"), title: "Copy", action: copyLinkToClipboard),
+            SocialNetworkSharing(icon: UIImage(named: "fb"), title: "Facebook", action: { self.shareToApp(urlScheme: "fb://composer?text=", webURL: "https://www.facebook.com/sharer/sharer.php?u=") }),
+            SocialNetworkSharing(icon: UIImage(named: "mess"), title: "Messenger", action: { self.shareToApp(urlScheme: "fb-messenger://share?link=", webURL: "https://www.messenger.com/t/") }),
+            SocialNetworkSharing(icon: UIImage(named: "tele"), title: "Telegram", action: { self.shareToApp(urlScheme: "tg://msg?text=", webURL: "https://t.me/share/url?url=") }),
+            SocialNetworkSharing(icon: UIImage(named: "insta"), title: "Instagram", action: { self.shareToApp(urlScheme: "instagram://story-camera", webURL: "https://www.instagram.com/") }),
+            SocialNetworkSharing(icon: UIImage(named: "sms"), title: "SMS", action: { self.shareToApp(urlScheme: "sms:?&body=", webURL: nil) }),
+            SocialNetworkSharing(icon: UIImage(named: "ic_twitter"), title: "X", action: { self.shareToApp(urlScheme: "twitter://post?message=", webURL: "https://twitter.com/intent/tweet?text=") }),
+            SocialNetworkSharing(icon: UIImage(named: "zalo"), title: "Zalo", action: {
+                self.shareToApp(urlScheme: "zalo://send?text=", webURL: "https://zalo.me/share?url=")
+            }),
+            SocialNetworkSharing(icon: UIImage(named: "ic_whatapp"), title: "WhatsApp", action: {
+                self.shareToApp(urlScheme: "whatsapp://send?text=", webURL: "https://api.whatsapp.com/send?text=")
+            }),
 
-// MARK: - UICollectionView Delegate & DataSource
-extension ShareLinkViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return shareItems.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ShareLinkCollectionViewCell.identifier, for: indexPath) as! ShareLinkCollectionViewCell
-        cell.configure(with: shareItems[indexPath.row])
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        shareItems[indexPath.row].action()
-        //        DispatchQueue.main.async {
-        //                    self.dismiss(animated: true)
-        //        }
-    }
-}
 
-// MARK: - Share Actions
-extension ShareLinkViewController {
-    private func openApp(urlScheme: String, webURL: String?) {
-        let shareURL = shareLink.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
-        let appURLString = "\(urlScheme)\(shareURL)"
-        
-        if let appURL = URL(string: appURLString), UIApplication.shared.canOpenURL(appURL) {
-            UIApplication.shared.open(appURL)
-        } else if let webURL = webURL, let fallbackURL = URL(string: "\(webURL)\(shareURL)") {
-            UIApplication.shared.open(fallbackURL)
-        }
-    }
+            SocialNetworkSharing(icon: UIImage(named: "ic_more"), title: "More", action: openActivityView)
+        ]
+        return allItems.filter { $0.title == "Copy" || $0.title == "More" || isAppInstalled(urlScheme: urlScheme(for: $0.title)) }
+    }()
     
-    private func openFacebook() {
-        self.dismiss(animated: true) { [weak self] in
-            guard let self else { return }
-            self.openApp(urlScheme: "fb://composer?text=", webURL: "https://www.facebook.com/sharer/sharer.php?u=")
-        }
-    }
-    
-    private func openTelegram() {
-        self.dismiss(animated: true) { [weak self] in
-            guard let self else { return }
-            self.openApp(urlScheme: "tg://msg?text=", webURL: "https://t.me/share/url?url=")
-        }
-    }
-    
-    private func shareToMessenger() {
-        self.dismiss(animated: true) { [weak self] in
-            guard let self else { return }
-            self.openApp(urlScheme: "fb-messenger://share?link=", webURL: "https://www.messenger.com/t/")
-        }
-    }
-    
-    private func shareToInstagram() {
-        self.dismiss(animated: true) { [weak self] in
-                guard let self else { return }
-            self.openApp(urlScheme: "instagram://story-camera", webURL: "https://www.instagram.com/")
+    private func urlScheme(for app: String) -> String {
+        switch app {
+        case "Facebook": return "fb://"
+        case "Messenger": return "fb-messenger://"
+        case "Telegram": return "tg://"
+        case "Instagram": return "instagram://"
+        case "X": return "twitter://"
+        case "WhatsApp": return "whatsapp://"
+        case "Zalo": return "zalo://"
+
+        default: return ""
         }
     }
     
     private func copyLinkToClipboard() {
-        self.dismiss(animated: true) { [weak self] in
-            guard let self else { return }
-            UIPasteboard.general.string = shareLink
+        UIPasteboard.general.string = shareLink
             let alert = UIAlertController(title: "Đã sao chép", message: "Link đã được sao chép vào clipboard!", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default))
-            present(alert, animated: true)
-        }
-    }
-    @objc func shareToSMS() {
-        self.dismiss(animated: true) { [weak self] in
-            guard let self else { return }
-            openApp(urlScheme: "sms:?&body=", webURL: nil)
-        }
-    }
-    @objc func shareToTwitter() {
-        self.dismiss(animated: true) { [weak self] in
-            guard let self else { return }
-            openApp(urlScheme: "twitter://post?message=", webURL: "https://twitter.com/intent/tweet?text=")
-        }
+            self.present(alert, animated: true)
         
     }
     
-    
+    @objc private func shareToApp(urlScheme: String, webURL: String?) {
+        let encodedLink = shareLink.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+        let appURL = URL(string: "\(urlScheme)\(encodedLink)")
+        let fallbackURL = webURL.flatMap { URL(string: "\($0)\(encodedLink)") }
+        dismiss(animated: true) {
+            
+            if let appURL = appURL, UIApplication.shared.canOpenURL(appURL) {
+                UIApplication.shared.open(appURL)
+            } else if let fallbackURL = fallbackURL {
+                UIApplication.shared.open(fallbackURL)
+            }
+        }
+        print("haha")
+    }
     private func openActivityView() {
         guard let presentingVC = self.presentingViewController else { return }
         
@@ -192,5 +124,23 @@ extension ShareLinkViewController {
             presentingVC.present(activityVC, animated: true)
         }
     }
+
+}
+
+// MARK: - UICollectionView Delegate & DataSource
+extension ShareLinkViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return shareItems.count
+    }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ShareLinkCollectionViewCell.identifier, for: indexPath) as! ShareLinkCollectionViewCell
+        cell.configure(with: shareItems[indexPath.row])
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+            DispatchQueue.main.async {
+                self.shareItems[indexPath.row].action()
+            }
+        }
 }
