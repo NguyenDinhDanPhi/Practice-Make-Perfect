3223b8f8f9000ebbdfaeb5a1e068f471542f12da
danphi <phittnh@gmail.com>
2025-04-03 15:33:41 +0700

    dropdown nhưng đẹp vãi

--- DemoNotification/DemoNotification/notify/DropDown/DropdownMenuViewController.swift
+++ DemoNotification/DemoNotification/notify/DropDown/DropdownMenuViewController.swift
@@ -5,6 +5,7 @@ class DropdownMenuViewController: UIViewController, UITableViewDataSource, UITab
     private var tableViewButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Select Option", for: .normal)
+        button.backgroundColor = .clear
         button.addTarget(self, action: #selector(onShowTableViewButtonPressed), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
@@ -12,17 +13,15 @@ class DropdownMenuViewController: UIViewController, UITableViewDataSource, UITab
     
     private var tableView: UITableView = {
         let table = UITableView()
-        table.layer.cornerRadius = 5
-        table.layer.borderColor = UIColor.systemGray5.cgColor
-        table.layer.borderWidth = 1.0
         table.isHidden = true
         table.translatesAutoresizingMaskIntoConstraints = false
+        table.separatorStyle = .none
         return table
     }()
     
     private var transparentView: UIView = {
         let view = UIView()
-        view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.2)
         view.isHidden = true
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
@@ -53,16 +52,8 @@ class DropdownMenuViewController: UIViewController, UITableViewDataSource, UITab
             tableViewButton.heightAnchor.constraint(equalToConstant: 40)
         ])
         
-        // Cài đặt Auto Layout cho tableView
         NSLayoutConstraint.activate([
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            tableView.heightAnchor.constraint(equalToConstant: 120) // Đặt chiều cao mặc định
-        ])
-        
-        // Cài đặt Auto Layout cho transparentView
-        NSLayoutConstraint.activate([
-            transparentView.topAnchor.constraint(equalTo: view.topAnchor),
+            transparentView.topAnchor.constraint(equalTo: tableView.bottomAnchor),
             transparentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             transparentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             transparentView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
@@ -84,15 +75,11 @@ class DropdownMenuViewController: UIViewController, UITableViewDataSource, UITab
     func addTableView(frames: CGRect) {
         transparentView.isHidden = false
         
-        // Tính toán chiều cao cho dropdown menu
-        let tableViewHeight = (items.count > 3) ? 120.0 : CGFloat(items.count) * 40
-        
-        // Đặt lại vị trí và kích thước cho tableView
         NSLayoutConstraint.activate([
             tableView.topAnchor.constraint(equalTo: tableViewButton.bottomAnchor, constant: 8),
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: frames.origin.x),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -frames.origin.x),
-            tableView.heightAnchor.constraint(equalToConstant: tableViewHeight)
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.heightAnchor.constraint(equalToConstant: 224)
         ])
         
         tableView.isHidden = false
@@ -111,8 +98,8 @@ class DropdownMenuViewController: UIViewController, UITableViewDataSource, UITab
     }
     
     // Hiển thị dropdown khi nhấn vào nút
-    @objc func onShowTableViewButtonPressed(_ sender: UIButton) {
-        addTableView(frames: sender.frame)
+    @objc func onShowTableViewButtonPressed(_ sender: UITapGestureRecognizer) {
+        addTableView(frames: tableViewButton.frame)
     }
 
     // MARK: - TableView DataSource & Delegate Methods
@@ -128,7 +115,7 @@ class DropdownMenuViewController: UIViewController, UITableViewDataSource, UITab
     }
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 40
+        return 48
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
