8f7432cb35cf032c739f3e05a625049fe7e76386
danphi <phittnh@gmail.com>
2025-04-04 08:09:51 +0700

    fix something

--- DemoNotification/DemoNotification/notify/NotificationViewController.swift
+++ DemoNotification/DemoNotification/notify/NotificationViewController.swift
@@ -12,15 +12,15 @@ class NotificationViewController: UIViewController {
         return button
     }()
     
-     lazy var dropdown: DropdownMenuView = {
+    lazy var dropdown: DropdownMenuView = {
         let view = DropdownMenuView()
         view.translatesAutoresizingMaskIntoConstraints = false
         view.isHidden = true  // Đặt mặc định là ẩn
         return view
     }()
     
-    private lazy var notifiListView: UIView = {
-        let view = UIView()
+    private lazy var notifiListView: NotificationsListView = {
+        let view = NotificationsListView()
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
@@ -31,8 +31,8 @@ class NotificationViewController: UIViewController {
         title = "Notification"
         setUpView()
         dropdown.removeDropdown = { [weak self] in
-                    self?.dropdown.removeFromSuperview()  // Loại bỏ dropdown
-                }
+            self?.dropdown.removeFromSuperview()  // Loại bỏ dropdown
+        }
     }
     
     func setUpView() {
@@ -41,59 +41,58 @@ class NotificationViewController: UIViewController {
             titleButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
             titleButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             
-          // Đặt chiều cao cho dropdown
         ])
         
         view.addSubview(notifiListView)
         
         NSLayoutConstraint.activate([
-            notifiListView.topAnchor.constraint(equalTo: titleButton.bottomAnchor, constant:  50),
+            notifiListView.topAnchor.constraint(equalTo: titleButton.bottomAnchor, constant:  8),
             notifiListView.leftAnchor.constraint(equalTo: view.leftAnchor),
             notifiListView.rightAnchor.constraint(equalTo: view.rightAnchor),
             notifiListView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
         
-        // Dữ liệu cho EmptyStateView
-        let data = EmptyStateViewData(numberOfButtons: .oneButton,
-                                      titleText: "Không Thể tải danh sách thông báo",
-                                      subtitleText: "vui lòng thử lại",
-                                      imageDetails: "empy",
-                                      textRetryButton: "tải lại", textBackButton: "", textExistButton: "")
-        
-        let emptyStateVC = EmptyStateViewController.createEmptyStateViewController(data: data)
-        emptyStateVC.addToParentViewController(self, in: notifiListView)
-        
-        emptyStateVC.onRetryAction  = { [weak self] in
-            print("Retrying...")
-        }
+//        // Dữ liệu cho EmptyStateView
+//        let data = EmptyStateViewData(numberOfButtons: .oneButton,
+//                                      titleText: "Không Thể tải danh sách thông báo",
+//                                      subtitleText: "vui lòng thử lại",
+//                                      imageDetails: "empy",
+//                                      textRetryButton: "tải lại", textBackButton: "", textExistButton: "")
+//        
+//        let emptyStateVC = EmptyStateViewController.createEmptyStateViewController(data: data)
+//        emptyStateVC.addToParentViewController(self, in: notifiListView)
+//        
+//        emptyStateVC.onRetryAction  = { [weak self] in
+//            print("Retrying...")
+//        }
         
         dropdown.didSelectOption = { [weak self] index in
             guard let self = self else { return }
             let title = self.dropdown.items[index].0
             self.titleButton.setTitle("\(title) ⌄", for: .normal)
-         //   self.toggleMenu()
+            //   self.toggleMenu()
         }
     }
     
     @objc func onShowTableViewButtonPressed() {
         if dropdown.superview == nil {
-                   // Nếu DropdownMenuView chưa được thêm vào view, ta thêm vào
-                   view.addSubview(dropdown)
-                   
-                   // Cài đặt Auto Layout cho DropdownMenuView khi thêm vào view
-                   NSLayoutConstraint.activate([
-                       dropdown.topAnchor.constraint(equalTo: titleButton.bottomAnchor, constant: 8),
-                       dropdown.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-                       dropdown.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-                       dropdown.bottomAnchor.constraint(equalTo: view.bottomAnchor )
-                   ])
-                   
-                   // Hiển thị dropdown
-                   dropdown.isHidden = false
-                   dropdown.addTableView(frames: titleButton.frame)
-               } else {
-                   // Nếu DropdownMenuView đã có trong view, ta sẽ loại bỏ nó
-                   dropdown.isHidden = true
-                   dropdown.removeFromSuperview()
-               }    }
+            // Nếu DropdownMenuView chưa được thêm vào view, ta thêm vào
+            view.addSubview(dropdown)
+            
+            // Cài đặt Auto Layout cho DropdownMenuView khi thêm vào view
+            NSLayoutConstraint.activate([
+                dropdown.topAnchor.constraint(equalTo: titleButton.bottomAnchor, constant: 8),
+                dropdown.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                dropdown.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                dropdown.bottomAnchor.constraint(equalTo: view.bottomAnchor )
+            ])
+            
+            // Hiển thị dropdown
+            dropdown.isHidden = false
+            dropdown.addTableView(frames: titleButton.frame)
+        } else {
+            // Nếu DropdownMenuView đã có trong view, ta sẽ loại bỏ nó
+            dropdown.isHidden = true
+            dropdown.removeFromSuperview()
+        }    }
 }
