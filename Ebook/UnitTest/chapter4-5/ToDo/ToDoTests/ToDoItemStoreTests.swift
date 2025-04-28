e97d539e8b8b3c6ff7191294c52eeb17f1e56756
phi <phindd@fpt.com>
2025-04-28 15:42:20 +0700

    test with combine - to page 98

--- Ebook/UnitTest/chapter4-5/ToDo/ToDoTests/ToDoItemStoreTests.swift
+++ Ebook/UnitTest/chapter4-5/ToDo/ToDoTests/ToDoItemStoreTests.swift
@@ -11,26 +11,52 @@ import Combine
 
 class ToDoItemStoreTests: XCTestCase {
     
-    func test_add_shouldPublishChange() {
+    func test_add_shouldPublishChange() throws {
         let sut = ToDoItemStore()
-        let publisherExpectation = expectation(description: "wait for publisher expectation")
-        var receivedItems: [ToDoItem] = []
-        
-        let token = sut.itemPublisher
+        let toDoItem = ToDoItem(title: "Dummy")
+        let receivedItems = try wait(for: sut.itemPublisher)
+        {
+            sut.add(toDoItem)
+        }
+        XCTAssertEqual(receivedItems, [toDoItem])
+    }
+    
+    func test_check_shouldPublishChangeInDoneItems() throws {
+        let sut = ToDoItemStore()
+        let toDoItem = ToDoItem(title: "Dummy")
+        sut.add(toDoItem)
+        sut.add(ToDoItem(title: "Dummy 2"))
+        let receivedItems = try wait(for: sut.itemPublisher)
+        {
+            sut.check(toDoItem)
+        }
+        let doneItems = receivedItems.filter({ $0.done })
+        XCTAssertEqual(doneItems, [toDoItem])
+    }
+}
+
+extension XCTestCase {
+    func wait<T: Publisher>(
+        for publisher: T,
+        afterChange change: () -> Void) throws
+    -> T.Output where T.Failure == Never {
+        let publisherExpectation = expectation(
+            description: "Wait for publisher in \(#file)"
+        )
+        var result: T.Output?
+        let token = publisher
             .dropFirst()
-            .sink { items in
-                receivedItems = items
+            .sink { value in
+                result = value
                 publisherExpectation.fulfill()
             }
-        let todoItem = ToDoItem(title: "Dummy")
-        sut.add(todoItem)
+        change()
         wait(for: [publisherExpectation], timeout: 1)
         token.cancel()
-        
-        XCTAssertEqual(receivedItems.first?.title,
-                       todoItem.title)
-        
-        
-        
+        let unwrappedResult = try XCTUnwrap(
+            result,
+            "Publisher did not publish any value"
+        )
+        return unwrappedResult
     }
 }
