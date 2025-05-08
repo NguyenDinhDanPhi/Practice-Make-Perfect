b8a7f5ce5c5be40cdd2934699f197255207103bf
phi <phindd@fpt.com>
2025-05-08 08:30:47 +0700

    .

---
+++ DemoRemoteLog/DemoRemoteLog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoRemoteLog
+//
+//  Created by Dan Phi on 8/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
