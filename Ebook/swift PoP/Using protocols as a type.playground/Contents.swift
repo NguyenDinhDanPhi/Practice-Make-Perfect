e7970941d71fcb1eea9b034d6171e723d5edd9b0
phi <phindd@fpt.com>
2025-06-05 14:14:09 +0700

    nhiều hơn về tính đa hình của protocol

--- Ebook/swift PoP/Using protocols as a type.playground/Contents.swift
+++ Ebook/swift PoP/Using protocols as a type.playground/Contents.swift
@@ -25,9 +25,40 @@ struct SwiftProgrammer: Person {
     }
 }
 
+struct FootballPlayer: Person {
+    var firstName: String
+    var lastName: String
+    var birthDate: Date
+    var profession: String {
+        return "hehe"
+    }
+    
+    init(firstName: String, lastName: String, birthDate: Date) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.birthDate = birthDate
+    }
+    
+    
+}
+
 var myPerson: Person
 
 myPerson = SwiftProgrammer(firstName: "jon", lastName: "DanPhi", birthDate: Date())
 
 print(myPerson)
 /// result: SwiftProgrammer(firstName: "jon", lastName: "DanPhi", birthDate: 2025-06-04 13:35:45 +0000)
+myPerson = FootballPlayer(firstName: "jon", lastName: "DanPhi", birthDate:  Date())
+///result: FootballPlayer(firstName: "jon", lastName: "DanPhi", birthDate: 2025-06-04 13:35:45 +0000)
+/// Nghĩa là myPerson có tính đa hình, có thể gán cho nhiều kiểu dữ liệu khác nhau (ví dụ như class/struct) miễn sao kiểu dữ liệu đó tuân thủ chung 1 protocol
+
+
+var programmer = SwiftProgrammer(firstName: "Nguyen", lastName: "Phi", birthDate: Date())
+var player = FootballPlayer(firstName: "DanPhi", lastName: "Phi", birthDate: Date())
+
+var people: [Person] = []
+people.append(player)
+people.append(programmer)
+
+print(people)
+///result: [main.FootballPlayer(firstName: "DanPhi", lastName: "Phi", birthDate: 2025-06-05 07:08:32 +0000), main.SwiftProgrammer(firstName: "Nguyen", lastName: "Phi", birthDate: 2025-06-05 07:08:32 +0000)]
