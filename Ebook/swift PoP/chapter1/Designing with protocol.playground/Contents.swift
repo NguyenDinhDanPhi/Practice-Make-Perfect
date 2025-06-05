ebbd403ce583faa786884843d1f7490a45854934
phi <phindd@fpt.com>
2025-06-05 17:21:27 +0700

    split design with protocols

---
+++ Ebook/swift PoP/chapter1/Designing with protocol.playground/Contents.swift
@@ -0,0 +1,53 @@
+import UIKit
+
+protocol RobotMovement {
+    func forward(speedPercent: Double)
+    func reverse(speedPercent: Double)
+    func left(speedPercent: Double)
+    func right(speedPercent: Double)
+    func stop()
+}
+
+protocol RobotMovementThreeDimensions: RobotMovement {
+    func up(speedPercent: Double)
+    func down(speedPercent: Double)
+}
+
+protocol Sensor {
+    var sensorType: String { get }
+    var sensorName: String { get set}
+    
+    init (sensorName: String)
+    func pollSensor()
+}
+        
+protocol EnvironmentSensor: Sensor {
+    func currentTemperature() -> Double
+    func currentHumidity() -> Double
+}
+
+protocol RangeSensor: Sensor {
+    func setRangeNotification(rangeCentimeter:  Double, rangeNotification:  () -> Void)
+    func currentRange() -> Double
+}
+
+protocol DisplaySensor: Sensor {
+    func displayMessage(_ message: String)
+}
+
+protocol WireLessSensor: Sensor {
+    func setMessageReceivedNotification(messageNotification:(String) -> Void)
+    func messageSend(message: String)
+}
+
+protocol Robot {
+    var name : String { get set }
+    var robotMovement : RobotMovement { get set }
+    var sensors : [Sensor] { get }
+    
+    init (name: String, robotMovement: RobotMovement)
+    
+    func addSensor(_ sensor: Sensor)
+    func pollSensors()
+}
+
