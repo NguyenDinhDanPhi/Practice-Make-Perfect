0f7e48bd3fc4f0dcec049b9dea05f9e194d2dad2
phi <phindd@fpt.com>
2025-06-12 10:17:37 +0700

    init demo HaishinKit

---
+++ Demo/DemoHaishinKit/DemoHaishinKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoHaishinKit
+//
+//  Created by Dan Phi on 12/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
