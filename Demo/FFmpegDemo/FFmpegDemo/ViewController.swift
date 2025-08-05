0469d7f6bbe874a75b66846db39bbba69d91ddff
phi <phindd@fpt.com>
2025-08-05 14:41:34 +0700

    add ffmpeg to project

---
+++ Demo/FFmpegDemo/FFmpegDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FFmpegDemo
+//
+//  Created by Dan Phi on 5/8/25.
+//
+
+import UIKit
+import ffmpegkit
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
