7f83f887474bc63ed24b8ade054ecfbb318bf416
phi <phindd@fpt.com>
2025-08-08 15:36:13 +0700

    init demo

---
+++ Demo/DemoUpLoadFireBase/DemoUpLoadFireBase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoUpLoadFireBase
+//
+//  Created by Dan Phi on 8/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
