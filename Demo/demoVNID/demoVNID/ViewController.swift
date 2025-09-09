3ec32a98bbeb103d9d478a7432eaf85bcde3cb51
phi <phindd@fpt.com>
2025-09-09 13:46:30 +0700

    init dem VNeID

---
+++ Demo/demoVNID/demoVNID/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  demoVNID
+//
+//  Created by Dan Phi on 9/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private lazy var button
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+    
+    
+
+}
+
